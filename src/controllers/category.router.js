c70a27b61a557e6bc83c82eaaa1fd021015571fc
Lana Schuster <lanaschuster1@gmail.com>
2021-06-30 10:44:13 -0300

    category documentation

--- src/controllers/category.router.js
+++ src/controllers/category.router.js
@@ -19,6 +19,46 @@ router.options('/:id', (req, res) => {
   res.status(200).end()
 })
 
+/**
+ * @swagger
+ * /api/categories:
+ *  get:
+ *    summary: Get category page
+ *    tags: [Categories]
+ *    parameters:
+ *      - in: query
+ *        name: page
+ *        type: integer
+ *        description: the page number (begins with 1)
+ *        minimum: 1
+ *        required: true
+ *      - in: query
+ *        name: size
+ *        type: integer
+ *        description: number of items per page
+ *        default: 5
+ *      - in: query
+ *        name: sort
+ *        type: string
+ *        description: field to be sorted by
+ *        default: name
+ *      - in: query
+ *        name: direction
+ *        type: string
+ *        description: direction of sorting (ASC or DESC)
+ *        default: ASC
+ *      - in: query
+ *        name: filter
+ *        type: string
+ *        description: search items where value like filter
+ *    responses:
+ *      200:
+ *        description: page of categories
+ *      401:
+ *        description: not authorized
+ *      500:
+ *        description: internal server error
+ */
 router.get('/', async (req, res, next) => {
   try {
     if (!req.query.page) {
@@ -39,6 +79,26 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+/**
+ * @swagger
+ * /api/categories/{id}:
+ *  get:
+ *    summary: Get category by id
+ *    tags: [Categories]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        type: integer
+ *        description: the category id
+ *        required: true
+ *    responses:
+ *      200:
+ *        description: ok
+ *      401:
+ *        description: not authorized
+ *      500:
+ *        description: internal server error
+ */
 router.get('/:id', async (req, res, next) => {
   try {
     const category = await Category.findById(req.params.id)
@@ -49,6 +109,26 @@ router.get('/:id', async (req, res, next) => {
   }
 })
 
+/**
+ * @swagger
+ * /api/categories:
+ *  post:
+ *    summary: Create a new category
+ *    tags: [Categories]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Category'
+ *    responses:
+ *      201:
+ *        description: category created
+ *      401:
+ *        description: not authorized
+ *      500:
+ *        description: internal server error
+ */
 router.post('/', async (req, res, next) => {
   let transaction
   
@@ -67,6 +147,33 @@ router.post('/', async (req, res, next) => {
   }
 })
 
+/**
+ * @swagger
+ * /api/categories/{id}:
+ *  put:
+ *    summary: Update category's data
+ *    tags: [Categories]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: the category id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Category'
+ *    responses:
+ *      204:
+ *        description: no content
+ *      401:
+ *        description: not authorized
+ *      500:
+ *        description: internal server error
+ */
 router.put('/:id', async (req, res, next) => {
   let transaction
   
@@ -85,4 +192,29 @@ router.put('/:id', async (req, res, next) => {
   }
 })
 
+/**
+ * @swagger
+ * tags:
+ *   name: Categories
+ *   description: API para cadastro de categoria de despesas e incomings
+ * components:
+ *   schemas:
+ *      Category:
+ *        type: object
+ *        required:
+ *          - name
+ *          - type
+ *          - description
+ *        properties:
+ *          id:
+ *            type: integer
+ *            description: auto-generated id
+ *          name:
+ *            type: string
+ *          description:
+ *            type: string
+ *          type:
+ *            type: string
+ *            description: value can be EXPENSE or INCOMING
+ */
 module.exports = router
